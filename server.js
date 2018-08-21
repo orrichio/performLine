5e9c4b36c5d066687e5b95b34fb608ff781e2bbe
Steve Orrichio <orrichio@gmail.com>
2018-08-20 21:32:14 -0400

    pathing issue

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname, 'dist', 'performline'));
 
-app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(__dirname, 'dist', 'performline', 'index.html'));
+app.get('/', function (req, res, next) {
+    res.sendfile('index.html', { root: __dirname + '/dist/performline' });
 });
 
 // Start the app by listening on the default Heroku port
