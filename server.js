5374411c79879e61d3a401ed64df8c54ef573cee
Steve Orrichio <orrichio@gmail.com>
2018-08-20 22:54:48 -0400

    Cors issue

--- server.js
+++ server.js
@@ -6,9 +6,23 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/performLine'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/performLine/index.html'));
+app.get('/*', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://performline.herokuapp.com/');
+    res.header("Access-Control-Allow-Origin", "*");
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    // next();
+    res.sendFile(path.join(__dirname, '/dist/performLine/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
